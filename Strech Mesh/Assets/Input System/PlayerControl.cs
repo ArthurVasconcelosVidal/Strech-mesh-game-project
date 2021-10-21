3477fc862f94dfb5dd7646016490e1b5c9a5c232
ArthurVasconcelosVidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2021-10-21 11:56:38 -0300

    Basic input

---
+++ Strech Mesh/Assets/Input System/PlayerControl.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input System/PlayerControl.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControl : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControl()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControl"",
+    ""maps"": [
+        {
+            ""name"": ""control"",
+            ""id"": ""a1036ce2-641a-49a6-9b62-596bf1832fa9"",
+            ""actions"": [
+                {
+                    ""name"": ""Moviment"",
+                    ""type"": ""Value"",
+                    ""id"": ""be4b1d7c-727c-40d7-bace-be525591ade2"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""29ec9e35-5c8f-45ab-af0d-7424858e21a2"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""96505f49-e43d-4d53-ab9d-40f2db8e14ff"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""be2ac715-edc6-40af-9357-dd9c439a5bde"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""ab1878a6-261d-4680-a5b9-c7a845f1f561"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""a4614774-17b8-479e-b65c-76e380586cec"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""14c9ac42-7a80-446e-9e8b-daa196a30a31"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Moviment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // control
+        m_control = asset.FindActionMap("control", throwIfNotFound: true);
+        m_control_Moviment = m_control.FindAction("Moviment", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // control
+    private readonly InputActionMap m_control;
+    private IControlActions m_ControlActionsCallbackInterface;
+    private readonly InputAction m_control_Moviment;
+    public struct ControlActions
+    {
+        private @PlayerControl m_Wrapper;
+        public ControlActions(@PlayerControl wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Moviment => m_Wrapper.m_control_Moviment;
+        public InputActionMap Get() { return m_Wrapper.m_control; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(ControlActions set) { return set.Get(); }
+        public void SetCallbacks(IControlActions instance)
+        {
+            if (m_Wrapper.m_ControlActionsCallbackInterface != null)
+            {
+                @Moviment.started -= m_Wrapper.m_ControlActionsCallbackInterface.OnMoviment;
+                @Moviment.performed -= m_Wrapper.m_ControlActionsCallbackInterface.OnMoviment;
+                @Moviment.canceled -= m_Wrapper.m_ControlActionsCallbackInterface.OnMoviment;
+            }
+            m_Wrapper.m_ControlActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Moviment.started += instance.OnMoviment;
+                @Moviment.performed += instance.OnMoviment;
+                @Moviment.canceled += instance.OnMoviment;
+            }
+        }
+    }
+    public ControlActions @control => new ControlActions(this);
+    public interface IControlActions
+    {
+        void OnMoviment(InputAction.CallbackContext context);
+    }
+}
--- Strech Mesh/Assets/Scripts/Player Scripts/InputManager.cs
+++ Strech Mesh/Assets/Scripts/Player Scripts/InputManager.cs
@@ -3,5 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour{
+    PlayerControl playerControl;
+    public Vector2 rightStick;
 
+    void Awake(){
+        playerControl = new PlayerControl();
+
+        playerControl.control.Moviment.performed += ctx => {
+            rightStick = ctx.ReadValue<Vector2>();
+        };
+
+        playerControl.control.Moviment.canceled += ctx =>{
+            rightStick = Vector2.zero;
+        };
+    }
+
+    void OnEnable(){
+        playerControl.control.Enable();
+    }
+
+    void OnDisable(){
+        playerControl.control.Disable();
+    }
 }
