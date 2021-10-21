d59f7a2360ca62017fbe5c7cc748bf151c59aa0c
ArthurVasconcelosVidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2021-10-21 11:42:07 -0300

    Base scripts / Essencial packages

---
+++ Strech Mesh/Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour{
+
+    public Rigidbody rigidbody;
+    public MovimentMamager movimentMamager;
+    public InputManager inputManager;
+    public ActionManager actionManager;
+
+
+}
